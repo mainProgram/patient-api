81260dd25799eaab1ca48b47c936ec141418de1d
devByJESUS <mmnledgar@gmail.com>
2025-05-13 22:40:23 +0000

    adding annotation

--- src/main/java/sn/fhunHospital/patient_api/security/WebSecurityConfig.java
+++ src/main/java/sn/fhunHospital/patient_api/security/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "X-CSRF-TOKEN"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
